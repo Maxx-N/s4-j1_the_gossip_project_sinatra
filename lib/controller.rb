abcd790b2236d1042515a90fb0ba10c80a429cfe
Maxx-N <maximenorval@gmail.com>
2019-10-21 20:46:14 +0200

    avec les commentaires, c'est mieux !

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,21 +1,21 @@
 require 'gossip'
 
-class ApplicationController < Sinatra::Base
+class ApplicationController < Sinatra::Base 
 
-  get '/' do
+  get '/' do # requête qui dirige l'utilisateur vers la page d'accueil (fichier "index.erb") lors de la saisie de l'URL ("http://localhost:4567/") dans le navigateur
     erb :index, locals: {gossips: Gossip.all}
   end
 
-  get '/gossips/new/' do
+  get '/gossips/new/' do # requête qui dirige l'utilisateur vers la page de création de potins (fichier "new_gossip.erb") lors de la saisie de l'URL ("http://localhost:4567/gossips/new/") dans le navigateur
     erb :new_gossip
   end
 
-  post '/gossips/new/' do
-    Gossip.new(params["gossip_author"],params["gossip_content"]).save
+  post '/gossips/new/' do # requête qui créé un potin en fonction du formulaire rempli par l'utilisateur
+    Gossip.new(params["gossip_author"],params["gossip_content"]).save # chaque gossip créé est stocké dans la base de données grâce à la méthode d'instance "save" définie dans la classe "Gossip"
     redirect '/'
   end
 
-  get '/gossips/:id' do
+  get '/gossips/:id' do # requête qui dirige l'utilisateur vers la page du potin (fichier "show.erb")portant le numéro de l'id lors de la saisie de l'URL ("http://localhost:4567/gossips/id") dans le navigateur
     puts "Voici le numéro du potin que tu veux : #{params['id']}!"
     puts Gossip.find(params['id'])
     erb :show, locals: {gossips: Gossip.all}
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -1,20 +1,20 @@
 require 'csv'
 
-class Gossip
+class Gossip   # classe "Model" qui va récupérer ou stocker des informations auprès de la base de données (contenue dans le fichier db/gossip.csv)
   attr_accessor :author, :content
 
-  def initialize(author, content)
+  def initialize(author, content) # une instance (un gossip) est initiée avec deux attributs : son auteur et son contenu
     @author = author
     @content = content
   end
 
-  def save
+  def save # chaque instance créée est stockée dans la base de données. Cette méthode est appelée par la requête "post" dans le fichier "controller.rb"
     CSV.open("./db/gossip.csv", "ab") do |csv|
       csv << [author, content]
     end
   end
 
-  def self.all
+  def self.all # chaque élément de la base de données (ou gossip) est stocké un par un dans l'array "all_gossips", ce qui permet de l'identifier par un index (sa position dans l'array)
     all_gossips = []
     CSV.read("./db/gossip.csv").each do |csv_line|
       all_gossips << Gossip.new(csv_line[0], csv_line[1])
@@ -22,9 +22,9 @@ class Gossip
     return all_gossips
   end
 
-  def self.find(id)
+  def self.find(id) # en appelant cette méthode avec un id comme argument, on peut retrouver un gossip grâce à son index (position dans l'array)
     all_gossips = self.all
-    return all_gossips[id.to_i - 1]
+    return all_gossips[id.to_i - 1] # les index commençant par 0 et non par un, on recherche l'index un rang en dessous de la position réelle (celle que l'on veut afficher)
   end
 
 end
